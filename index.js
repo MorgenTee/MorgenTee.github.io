bee8d7c706a31487662c9fbd1269320f7772d76c
Martin <Morgentee@protonmail.com>
2022-03-10 19:01:15 +0100

    counters

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 // Presale setup
 // smartbchtestnet
+const tokenSymbol = 'NFTC';
 const tokenAddress = '0x855E56ADD0F10B6b15bAcC1c0Dda75C269295244';
 const tokenSaleAddress = '0x81aBde1AbA337563A062f6890Cda05Ef1F146Cad';
 
 
 // const pricePerWei = 46640000000000; // 0.00004664 BCH -- real presale price
 const pricePerWei = 466400000000; // 0.0000 00 4664 BCH -- testnet presale price
+
+const presaleSupply = 2144082; // 2.14... million
+
+
 
 const bnPricePerWei = new BigNumber(pricePerWei);
 // constants
@@ -27,22 +32,62 @@ if (window.ethereum) {
   setTimeout(handleEthereum, 3000); // 3 seconds
 }
 
-function handleEthereum() {
+async function handleEthereum() {
   const { ethereum } = window;
   if (ethereum && ethereum.isMetaMask) {
     console.log('Ethereum successfully detected!');
     ethereum.request({ method: 'eth_requestAccounts' })
     const web3 = new Web3(Web3.givenProvider);
+
     TokenSale = new web3.eth.Contract(TokenSale_json_interface["abi"], tokenSaleAddress);
-    web3.eth.getBlockNumber().then(function (result) {
-      currentBlock = parseInt(result);
-    });
+
+    tokensSold = await TokenSale.methods.tokensSold().call();
+    tokensRemaining = presaleSupply - tokensSold;
+    $("#supply").html('<b>' + tokensRemaining + ' / ' + presaleSupply + ' ' + tokenSymbol + '</b> remaining');
+
+
+    // web3.eth.getBlockNumber().then(function (result) {
+    //   currentBlock = parseInt(result);
+    // });
   } else {
     $("#found").text(`MetaMask not found!`);
     console.log('Please install MetaMask!');
   }
 }
 
+
+async function addTokenToMetaMask() {
+  // const tokenAddress = '0xd00981105e61274c8a5cd5a88fe7e037d935b513'; // use the one defined above
+  // const tokenSymbol = 'NFTC'; // use the one defined above
+  const tokenDecimals = 18;
+  const tokenImage = ''; // need a logo
+  // const tokenImage = 'http://placekitten.com/200/300'; // need a logo
+
+  try {
+    // wasAdded is a boolean. Like any RPC method, an error may be thrown.
+    const wasAdded = await ethereum.request({
+      method: 'wallet_watchAsset',
+      params: {
+        type: 'ERC20', // Initially only supports ERC20, but eventually more!
+        options: {
+          address: tokenAddress, // The address that the token is at.
+          symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+          decimals: tokenDecimals, // The number of decimals in the token
+          image: tokenImage, // A string url of the token logo
+        },
+      },
+    });
+
+    if (wasAdded) {
+      console.log('Thanks for your interest!');
+    } else {
+      console.log('Your loss!');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+}
 
 
 
@@ -61,8 +106,8 @@ function showCost() {
   let tokensToBuy = 0;
   if ("" !== tokensToBuyString) tokensToBuy = parseInt(tokensToBuyString);
   const nTokensToBuy = tokensToBuy;
-  let cost = nTokensToBuy * pricePerWei * 10**-18; 
-  $("#cost").html('Cost ' + cost + ' BCH');
+  let cost = Math.round(nTokensToBuy * pricePerWei * 10 ** -12) * 10 ** -6; // get rid of stupid numbers at the end of the value with round
+  $("#cost").html('Cost: <b>' + cost + ' BCH</b>');
 }
 
 
@@ -75,6 +120,10 @@ async function requestpayment() {
   if ("" !== tokensToBuyString) tokensToBuy = parseInt(tokensToBuyString);
   const bnTokensToBuy = new BigNumber(tokensToBuy);
 
+
+  tokensSold = await TokenSale.methods.tokensSold().call();
+  tokensRemaining = presaleSupply - tokensSold;
+  $("#supply").html(tokensRemaining + ' / ' + presaleSupply + ' tokens remaining');
 
   console.log(`Buyer buys ${tokensToBuy} tokens`);
   await TokenSale.methods.buyTokens(bnTokensToBuy).send(

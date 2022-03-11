c5146535458e3e00944da5ea74a50a5d36d8f7ba
Martin <Morgentee@protonmail.com>
2022-03-11 10:18:27 +0100

    Looks much nicer now

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ const tokenAddress = '0x855E56ADD0F10B6b15bAcC1c0Dda75C269295244';
 const tokenSaleAddress = '0x81aBde1AbA337563A062f6890Cda05Ef1F146Cad';
 
 
+const presaleSupply = 2144082; // 2.14... million
+var tokensRemaining = presaleSupply;
+
 // const pricePerWei = 46640000000000; // 0.00004664 BCH -- real presale price
 const pricePerWei = 466400000000; // 0.0000 00 4664 BCH -- testnet presale price
-
-const presaleSupply = 2144082; // 2.14... million
-
-
-
 const bnPricePerWei = new BigNumber(pricePerWei);
 // constants
 const divideForsBCH = 10 ** 18;
@@ -41,10 +39,7 @@ async function handleEthereum() {
 
     TokenSale = new web3.eth.Contract(TokenSale_json_interface["abi"], tokenSaleAddress);
 
-    tokensSold = await TokenSale.methods.tokensSold().call();
-    tokensRemaining = presaleSupply - tokensSold;
-    $("#supply").html('<b>' + tokensRemaining + ' / ' + presaleSupply + ' ' + tokenSymbol + '</b> remaining');
-
+    updateSupply();
 
     // web3.eth.getBlockNumber().then(function (result) {
     //   currentBlock = parseInt(result);
@@ -105,25 +100,28 @@ function showCost() {
   console.log(tokensToBuyString);
   let tokensToBuy = 0;
   if ("" !== tokensToBuyString) tokensToBuy = parseInt(tokensToBuyString);
-  const nTokensToBuy = tokensToBuy;
+  const nTokensToBuy = Math.min(tokensToBuy, tokensRemaining);
+  $("#tokensToBuy").val(nTokensToBuy);
   let cost = Math.round(nTokensToBuy * pricePerWei * 10 ** -12) * 10 ** -6; // get rid of stupid numbers at the end of the value with round
   $("#cost").html('Cost: <b>' + cost + ' BCH</b>');
+  return nTokensToBuy;
+}
+
+async function updateSupply() {
+  const tokensSold = await TokenSale.methods.tokensSold().call();
+  tokensRemaining = presaleSupply - tokensSold;
+  if (0 >= tokensRemaining) { $('#buyButton').prop('disabled', true); } else {$('#buyButton').prop('enabled', true);}
+  $("#supply").html(tokensRemaining + ' / ' + presaleSupply + ' tokens remaining');
 }
 
 
 async function requestpayment() {
   $("#cost").text = `Processing payment... `;
 
-  let tokensToBuyString = $("#tokensToBuy").val();
-  console.log(tokensToBuyString);
-  let tokensToBuy = 0;
-  if ("" !== tokensToBuyString) tokensToBuy = parseInt(tokensToBuyString);
+  updateSupply();
+
+  let tokensToBuy = await showCost();
   const bnTokensToBuy = new BigNumber(tokensToBuy);
-
-
-  tokensSold = await TokenSale.methods.tokensSold().call();
-  tokensRemaining = presaleSupply - tokensSold;
-  $("#supply").html(tokensRemaining + ' / ' + presaleSupply + ' tokens remaining');
 
   console.log(`Buyer buys ${tokensToBuy} tokens`);
   await TokenSale.methods.buyTokens(bnTokensToBuy).send(
@@ -131,17 +129,9 @@ async function requestpayment() {
       from: ethereum.selectedAddress,
       value: bnTokensToBuy.times(bnPricePerWei)
     }
-  );
-  // .then(() => {
-  //   console.log('TokenSale: Sale is active: ' + await TokenSale.saleActive());
-  //   console.log('TokenSale sells tokens for ' + (await TokenSale.tokenPrice() * 10 ** -18).toString() + ' BCH per token');
-  //   console.log((await TokenSale.tokensSold()).toString() + ' / ' + presaleSupply + ' tokens sold');
-  //   console.log('TokenSale has ' + (await SimpleToken.balanceOf(tokenSaleAddress) * 10 ** -18).toString() + ' tokens left');
-  //   console.log('TokenSale contract currently has ' + (await ethers.provider.getBalance(TokenSale.address) * 10 ** -18).toString() + ' sBCH');
-
-
-  // });
-  // .then(() => {
-  //   $("#cost").text(`Transaction finished.`);
-  // });
+  )
+    .then(() => {
+      $("#cost").text(`Transaction finished.`);
+      updateSupply();
+    });
 }

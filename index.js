931f597d12200441909643bd5e09e9d4c68c8586
MorgenTee <MorgenTee@protonmail.com>
2022-03-15 11:18:35 +0100

    fixed

--- index.js
+++ index.js
@@ -102,8 +102,8 @@ function showCost() {
   console.log(tokensToBuyString);
   let tokensToBuy = 0;
   if ("" !== tokensToBuyString) tokensToBuy = parseInt(tokensToBuyString);
-  const nTokensToBuy = Math.min(tokensToBuy, tokensRemaining, maxTokensPerTx);
-  if (100 < tokensToBuy) tokensToBuy = 100;
+  var nTokensToBuy = Math.min(tokensToBuy, tokensRemaining, maxTokensPerTx);
+  if (100 < nTokensToBuy) nTokensToBuy = 100;
   $("#tokensToBuy").val(nTokensToBuy);
   let cost = Math.round(nTokensToBuy * pricePerWei * 10 ** -12) * 10 ** -6; // get rid of stupid numbers at the end of the value with round
   $("#cost").html('Cost: <strong>' + cost + ' BCH</strong>');

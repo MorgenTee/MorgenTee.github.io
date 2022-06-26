d9e824eaadf433abdba06e60b412a1473109d82e
Martin <Morgentee@protonmail.com>
2022-06-27 00:57:03 +0200

    minor fixes, moved to docs

---
+++ docs/js/settings-test-martin.js
@@ -0,0 +1,14 @@
+// Settings for testnet (SmartBCH Amber, NetID 10001)
+// deployed on testnet from 0xE0d61627d14F1E6C1C5FABDD219dF0a003C9a0cC
+
+window.PowSellerAddress    = '0x86691A01320dfDef8d97Cb38d9C7026efb242C12';
+window.PowTokenAddress     = '0x937962cF5565C203c50BaB45fd649f41ba5C9E2A';
+window.RewardTokenAddress  = '0x30C977f494caD30342c9Fe10353eC1Ffb0b105B8';
+window.WomNftAddress       = '0x82DB6802580CE0c06eedB59c0761D97218e7679E';
+window.FlexUsdAddress      = '0xd0e6466293c6cda8caa96a57683909383c01411d';
+window.StakingAddress      = '0xc6Db3e7d8Ea68feDf238670474A92cE58A8E2c21'; 
+window.DevAddress          = '0xE0d61627d14F1E6C1C5FABDD219dF0a003C9a0cC';
+window.rpcServer           = 'https://moeing.tech:9545';
+window.netId               = 10001;
+window.netName             = "SmartBch Testnet";
+window.minHarvest          = 1;
